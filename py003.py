2d9092035fdafb83725deadf29b03df450dcaebf
xinyicheng <214675320@qq.com>
2017-04-07 17:34:24 +0800

    Update py003.py

--- py003.py
+++ py003.py
@@ -3,6 +3,6 @@ import unittest
 from cxytest1 import*
 class Test001(unittest.TestCase):
   def test_cxy1(self):
-    self.assertEquals(Dowork(4,6,7),(23,2))
+    self.assertEquals(Dowork(4,6,7),(23,1))
   def test_cxy2(self):
     self.assertEquals(Dowork(1,2,13),(0,0))
