933a7dd74c19b6796848bc19f5467fd58e7c2c41
xinyicheng <214675320@qq.com>
2017-07-19 17:39:38 +0800

    Update py003.py

--- py003.py
+++ py003.py
@@ -6,3 +6,5 @@ class Test001(unittest.TestCase):
     self.assertEquals(Dowork(4,6,7),(23,1))
   def test_cxy2(self):
     self.assertEquals(Dowork(1,2,13),(0,0))
+if __name__ =="__main__":
+   unittest.mian()
